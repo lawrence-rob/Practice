fd42384202156a9816bd3c4b2287e27616a5f399
lawrence-rob <lawrencerobc@gmail.com>
2017-08-27 12:32:43 -0700

    C# Unity Practice

---
+++ Maze/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerController : MonoBehaviour {
+
+	private Rigidbody rb;
+	public float speed;
+	private int count;
+	public Text winText;
+
+
+	// Use this for initialization
+	void Start () {
+		rb = GetComponent<Rigidbody> ();
+		count = 0;
+		setWinText ();
+		winText.text = "";
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		float moveHorizontal = Input.GetAxis ("Horizontal");
+		float moveVertical = Input.GetAxis ("Vertical");
+		Vector3 movement = new Vector3 (moveHorizontal, 0.0f, moveVertical);
+		rb.AddForce (movement * speed);
+	}
+
+	void OnTriggerEnter(Collider other){
+		if (other.gameObject.CompareTag ("Pick Up")) {
+			other.gameObject.SetActive (false);
+			count = count + 1;
+			rb.gameObject.SetActive (false);
+			setWinText ();
+		}
+		if (other.gameObject.CompareTag ("Edge")) {
+			rb.transform.position = new Vector3 (11.7f, 0.5f, -11.6f);
+		}
+	}
+
+	void setWinText(){
+		if (count >= 1) {
+			winText.text = "You Win!";
+		}
+	}	
+
+}
